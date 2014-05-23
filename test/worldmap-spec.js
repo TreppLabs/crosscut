45305b35e63b5b77475bc8fefdf3e2641e4c3b70
Simon-o-Matic <simonraikallen@gmail.com>
2014-05-24 09:43:36 +1000

    Added a test file for worldmap testing.

---
+++ test/worldmap-spec.js
@@ -0,0 +1,19 @@
+// worldmap testing
+var worldmap = require('../lib/worldmap');
+worldmap.setConfig({tileHeight: 10, tileWidth: 10});
+	
+describe("worldmap", function() {
+	it("move 0x0", function() {
+		var move = worldmap.move(0,0,"#red");
+		var cell = worldmap.getCell(0,0);
+		expect(cell.color).toBe("#red");
+		expect(move).not.toBe(false);
+	});
+	it("same cell twice", function() {
+		var move1 = worldmap.move(0,0,"#red");
+		var move2 = worldmap.move(0,0,"#blue");
+		var cell = worldmap.getCell(0,0);
+		expect(cell.color).toBe("#red");
+		expect(move2).toBe(false);
+	});
+});
