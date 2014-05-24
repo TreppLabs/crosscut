ddb4538384b63a1725470d88ae15d00427336066
Simon-o-Matic <simonraikallen@gmail.com>
2014-05-24 17:26:22 +1000

    Enhanced the worldmap test.

--- test/worldmap-spec.js
+++ test/worldmap-spec.js
@@ -3,19 +3,28 @@ var worldmap = require('../lib/worldmap');
 worldmap.setConfig({tileHeight: 10, tileWidth: 10});
 	
 describe("worldmap", function() {
+	beforeEach(function() {
+		worldmap.theBigCrunch();
+	});
 	it("move 0x0", function() {
-		var move = worldmap.move(0,0,"#red");
-		var cell = worldmap.getCell(0,0);
-		expect(cell.color).toBe("#red");
-		expect(move).not.toBe(false);
+		var cell = worldmap.getCell(0,0);		// get the cell color
+		expect(cell.color).not.toBe("#red");	// its not red (its empty)
+		var move = worldmap.move(0,0,"#red");	// click the cell to red
+		cell = worldmap.getCell(0,0); 			// WHY DO I NEED TO DO THIS??!!? WHO IS OVERWRITING THIS
+		expect(cell.color).toBe("#red");		// now it should be red
+		expect(move).not.toBe(false);			// and should not have failed being set
 	});
-	it("same cell twice", function() {
-		var move1 = worldmap.move(0,0,"#red");
-		var move2 = worldmap.move(0,0,"#blue");
-		expect(move2).toBe(false);
-		
-		var cell = worldmap.getCell(0,0);
-		expect(cell.color).toBe("#red");
-		
-	});
+
+	it("move on non-empty location", function () {
+		var cell = worldmap.getCell(0,0);		// get a cell
+		expect(cell.color).not.toBe("#red");	// its not blue
+		expect(cell.color).not.toBe("#blue");	// and its not red
+		var move = worldmap.move(0,0,"#red");	// click it red
+		expect(move).not.toBe(false);			// this should work
+		cell = worldmap.getCell(0,0);			
+		expect(cell.color).toBe("#red");		// and the cell should be red
+		var move = worldmap.move(0,0,"#blue");	// try to make it blue
+		expect(move).toBe(false);				// this should fail
+		expect(cell.color).toBe("#red");		// and it should still be red
+	})
 });
