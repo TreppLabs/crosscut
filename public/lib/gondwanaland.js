ae439e00487469f580953ecbb12dd43608ea9ddc
Simon-o-Matic <simonraikallen@gmail.com>
2014-05-28 21:15:21 +1000

    After much futzing around and adding one and then subtracting one I think I finally got the click to be in the correct position and the grid to fill the area. Woot. Next to submit it to the server.

--- public/lib/gondwanaland.js
+++ public/lib/gondwanaland.js
@@ -23,8 +23,12 @@ var gondwanaland = (function() {
 	var canvas = $("#gondwanaland")[0];
 	var ctx = canvas.getContext("2d");
 
-	var cHeight = canvas.height;
-	var cWidth = canvas.width;
+	var cHeight;
+	var cWidth;
+
+	// how many cells fit on the page?
+	var cellsX = 0;
+	var cellsY = 0;
 
 	// where we are currently looking (units are accretions coords)
 	var vx = 50;
@@ -47,8 +51,12 @@ var gondwanaland = (function() {
 		zoom = level;
 
 		// how big to draw a cell?
-		cellWidth = CELL_W * zoom;
-		cellHeight = CELL_H * zoom;
+		cellWidth = Math.ceil(CELL_W * zoom);
+		cellHeight = Math.ceil(CELL_H * zoom);
+
+		// how many cells fit on the page?
+		cellsX = Math.ceil(cWidth/cellWidth);
+		cellsY = Math.ceil(cHeight/cellHeight);
 	}
 
 	// x,y cell coords that should be in the middle of our screen
@@ -67,16 +75,13 @@ var gondwanaland = (function() {
 	// Maybe only draw the whole then when we move. Otherwise its spot changes only
 	// Unless this is cheap enough.
 	function draw() {
-		// how many cells fit on the page?
-		var cellsX = cWidth/cellWidth;
-		var cellsY = cHeight/cellHeight;
 
 		ctx.clearRect(0,0,cWidth, cHeight);
-		ctx.strokeStyle = "#22ff33"; // the matrix style colors for cool effect. Or not.
-     
+
+		// step over each cell on the screen
 		for (var a = 0; a < cellsX; a++) {
 			for (var b = 0; b < cellsY; b++) {
-				// get the global x,y from our view port
+				// get the accretion global coords (agc) based on our view port
 				var gx = a + (vx - Math.floor(cellsX/2));
 				var gy = b + (vy - Math.floor(cellsY/2));
 
@@ -93,11 +98,39 @@ var gondwanaland = (function() {
 		var cxy = tileXYFromGXY(x,y);
 
 		if (!tile) { 
-			ctx.strokeRect(a*cellWidth, cHeight - b*cellHeight, cellWidth, cellHeight);
+			outlineCell(a,b, "#22ff33");
 		} else {
-			ctx.fillStyle = tile.colors[cxy.x][cxy.y].color;
-			ctx.fillRect(a*cellWidth+1, cHeight-b*cellHeight+1, cellWidth-1, cellHeight-1);
+			fillCell(a,b, tile.colors[cxy.x][cxy.y].color);
 		}					
+	}
+
+	function userMove(event) {
+		var xy = utils.getClickPosition(event, canvas);
+
+		// translate click position to accretion coords.
+		var clickedX = Math.floor(xy.x / cellWidth);
+		var clickedY = Math.floor((cHeight - xy.y)/ cellHeight); // flip the Y 
+
+		console.log("X Y cell click ["+(xy.y/ cellHeight)+"] (" + xy.x+","+xy.y+ ") " + clickedX + "," + clickedY);
+
+		// Locally change the color
+		fillCell(clickedX, clickedY, "#33ff44");
+
+		// TODO: Send to the server
+	}
+
+	// Draw a filled cell at viewport coordinates (in agc botom left = 0,0) 
+	// hence we need to convert to canvas coord which are top left = 0,0
+	function fillCell(x, y, color) {
+		var border = 1; // px around each cell
+		ctx.fillStyle = color;
+		ctx.fillRect(x * cellWidth+border, cHeight-(y+1)*cellHeight+border, cellWidth-1, cellHeight-1);
+	}
+
+	function outlineCell(x,y,color) {
+		var border = 1; // px around each cell
+		ctx.strokeStyle = "#22ff33"; // the matrix style colors for cool effect. Or not.
+		ctx.strokeRect(x * cellWidth, cHeight-(y+1)*cellHeight, cellWidth, cellHeight);		
 	}
 
 	/////////////////////////
@@ -148,43 +181,25 @@ var gondwanaland = (function() {
 	window.addEventListener("keyup", function(e){
 		switch(e.keyCode)
 		{
-			case 37: // left arrow
-				
-				break;
-			case 38: // up arrow
-				
-				break;
-			case 39: // right arrow
-				
-				break;
-			case 40: // down arrow
-				
-				break;
-			case 80: // key P pauses the game
-				
-				break;		
 			case 16: // key P pauses the game
 				shiftOn = false;
 				break;		
 		}
 	}, false);
 
-	function userMove(event) {
-		var xy = utils.getClickPosition(event, canvas);
-
-		// TODO: translate to accretion coords.
-		console.log("X Y CLICK: " + xy.x + "," + xy.y);
-	}
-
 	function resize() {
 		cHeight = canvas.height = $("#mapContainer").height();
   		cWidth = canvas.width =  $("#mapContainer").width();
+
+		// how many cells fit on the page?
+		setZoom(zoom);
+
   		draw();
 	} 
 
 	function init() {
-		resize();
 		setZoom(1);
+		resize();
 
 		canvas.addEventListener("mousedown", userMove, false);
 	}
